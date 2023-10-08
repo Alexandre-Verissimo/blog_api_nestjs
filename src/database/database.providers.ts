a326964ae3d3c5d9123786c752b6afcf1a015296
Alexandre-Verissimo <alexandre.nike10@hotmail.com>
2023-10-08 18:54:57 -0300

    primeiro commit

---
+++ src/database/database.providers.ts
@@ -0,0 +1,24 @@
+
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5441,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'blog_project',
+        entities: [
+            __dirname + '/../**/*.entity{.ts,.js}',
+        ],
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
